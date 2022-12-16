3fd4c1e53f809edf65d1b3fda025687620ac317f
karfaisarra <karfaisarra@gmail.com>
2022-12-16 18:06:09 +0100

    add changes

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -20,7 +20,30 @@
         </div>
     </div>
 </section>
-<section>
-
+<section class="banner py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col d-flex align-items-center">
+                <img width="70" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                <span class="text-uppercase text-white ps-3">Digital comics</span>
+            </div>
+            <div class="col d-flex align-items-center">
+                <img width="70" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <span class="text-uppercase text-white ps-3">Dc Merchandise</span>
+            </div>
+            <div class="col d-flex align-items-center">
+                <img width="70" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <span class="text-uppercase text-white ps-3">Subscription</span>
+            </div>
+            <div class="col d-flex align-items-center">
+                <img width="70" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                <span class="text-uppercase text-white ps-3">Comic Shop Locator</span>
+            </div>
+            <div class="col d-flex align-items-center">
+                <img width="70" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                <span class="text-uppercase text-white ps-3">Dc power visa</span>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
